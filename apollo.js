ab0d3633551f3db8e5b6442f625d50da31b08147
PureMax <ertr777@naver.com>
2021-04-22 15:21:22 +0900

    서버 주소 수정

--- apollo.js
+++ apollo.js
@@ -1,6 +1,6 @@
 const apolloClientOptions = {
-  // uri: "https://slog-deeptime-backend.herokuapp.com",
-  uri: `http://${Platform.OS === "ios" ? "localhost" : "192.168.0.229"}:4000`,
+  uri: "https://slog-deeptime-backend.herokuapp.com",
+  // uri: `http://${Platform.OS === "ios" ? "localhost" : "192.168.0.229"}:4000`,
 }
 
 export default apolloClientOptions
